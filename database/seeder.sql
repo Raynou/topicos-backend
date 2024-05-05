46514fab2972af79693800f9ce862e468fc6ab5f
Raymonds <rafaelgame2012@gmail.com>
2024-05-05 16:53:40 -0600

    fix syntax error in seeder.sql

--- database/seeder.sql
+++ database/seeder.sql
@@ -3,7 +3,7 @@ USE TOPICOS_DB;
 -- estar sujetos a cambios
 
 START TRANSACTION;
-    INSERT INTO RUTA (nombre, punto_inicial_lat, punto_final_lon, punto_final_lat, punto_final_lon) VALUES 
+    INSERT INTO RUTA (nombre, punto_inicial_lat, punto_inicial_lon, punto_final_lat, punto_final_lon) VALUES 
     ('Tampico Playa', '1.0', '1.0', '1.0', '1.0');
     INSERT INTO ARDUINO (ruta, numero_unidad) VALUES (1, 1);
 COMMIT;
